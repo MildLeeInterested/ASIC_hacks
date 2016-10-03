c316f8e7e38932e391a03ac4fccfe798c52a3628
MildLeeInterested <comfortableandrelaxed@yahoo.com.au>
2016-10-03 19:33:22 +0800

    Update ASIC_writeAndRead.ino
    Minor deletions and optimisations.  No functional changes.

--- ASIC_writeAndRead.ino
+++ ASIC_writeAndRead.ino
@@ -63,19 +63,17 @@ int fault = 0;// fault codes set by RECEIVE function.
 //1 bit at 9600 baud = 1000000/9600 = 104us.
 //try adding delayMicroseconds(104) between bytes???
 
-int bitLength = 104;//104;
+int bitLength = 104;
 
 unsigned long messageInt = 30;
 //Minumum interval between messages on the ASIC system bus is 25ms.
 //If there is no activity on the bus after this interval, it is safe to transmit.
 
-
-
-unsigned int node_ePAD = 65266;//Holds the node address of the ePAD HMI device = 65266.
+//unsigned int node_ePAD = 65266;//Holds the node address of the ePAD HMI device = 65266.
 unsigned int node_7040 = 32101;//Holds the node address of the ASIC/2 7040 PLC = 32101.(7D65)
 unsigned int node_8540 = 32102;//Holds the node address of the ASIC/2 8540 PLC = 32102.(7D66)
 
-unsigned int node_this = 45000;//32103;//This node address (7D67)(AFC8)
+unsigned int node_this = 45000;//This node address (AFC8)
 
 byte message[18];            // An array to hold the response message.
 int messageSize = sizeof(message);
@@ -131,8 +129,6 @@ void setup()
   {
     ACtemp[x] = 0;
   }
-
-
 
   // Open serial communications on hardware serial port
   Serial.begin(57600);
@@ -359,7 +355,6 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
 //byte 17 = checksum.  Lowest byte of simple addition of all previous bytes.
 
 {
-
   int resetCaseValue = 9;       //reset the master sequence.
   int incrementCaseValue = indexNOW + 1;   //move on to the next sequence step.
 
@@ -402,7 +397,6 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
   message[index] = ASICserial.read();//get the last byte from the buffer.  This will be the checksum if this is a valid reply message.
 
   int x = 0;
-  
 
   //**** error checking  ****
 
@@ -415,7 +409,6 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
     return resetCaseValue;
   }
 
-
   unsigned int calAddress = (message[1] << 8) + message[2];
   if (calAddress != rxAddress)
     //Check that the destination address of the message received matches the node address of the ePAD HMI.
@@ -425,10 +418,6 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
     fault = 4;
     return resetCaseValue;
   }
-
-
-
-
 
   calAddress = (message[HItxNode] << 8) + message[LOtxNode];
   if (calAddress != txAddress1 && calAddress != txAddress2)
@@ -440,8 +429,6 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
     return resetCaseValue;
   }
 
-
-
   if (checkSum != message[checksumByte])
     //Check that the check sum is correct.
     //If not, terminate with fault code.
@@ -450,10 +437,6 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
     fault = 6;
     return resetCaseValue;
   }
-
-
-
-
 
   if (message[objectByte] != 0x05 && message[objectByte] != 0x21)
     //Check that the object byte of the message received is the correct type 
@@ -473,48 +456,11 @@ int RECEIVE (int indexNOW, int rxAddress, int txAddress1, int txAddress2)
     fault = 9;
     return resetCaseValue;
   }
-
+  
   //If the code reaches this point, these's a good chance that this is a valid message.
-
   ASICserialFlush();
   fault = 0;
   delay(1);
-  return incrementCaseValue;
-}
-
-
-int DISPLAY1(int indexNOW)//shows raw data stream.  Not used.
-{
-  int incrementCaseValue = indexNOW + 1;   //move on to the next sequence step.
-
-  if (message[objectByte] == 0x05)
-  {
-    Serial.println(" ");
-    Serial.print("input: ");
-    Serial.print(message[instanceByte],HEX);
-    int temperature = (message[HIdataByte] << 8) + message[LOdataByte];
-    if (temperature == byte(0x0000))
-    {
-      Serial.println(" digital: OFF ");
-    }
-    else if (temperature == 0xFFFF)
-    {
-      Serial.println(" digital: ON ");
-    }
-    else
-    {
-      Serial.print(" data: ");
-      Serial.print(temperature / 100);
-      Serial.print(".");
-      Serial.print(temperature % 100);
-      Serial.println(" degrees ");
-    }
-  }
-  else if (message[objectByte] == 0x21)
-  {
-    Serial.println(" ");
-    Serial.println(" monitor block ");
-  }
   return incrementCaseValue;
 }
 
@@ -533,7 +479,6 @@ int STORE (int indexNOW, int TX1, int TX2)
      *AC4     = index 4, TX1, instance 9;
      *AC6     = index 6, TX2, instance 0;
      *factory = index 0, TX2, instance 3;
-     *
      */
 
     if (address == TX1)
@@ -583,20 +528,20 @@ int BUILD_QUERY (int indexNOW, byte AC)
   //Most of these values are static and are lifted directly from the message structure
   //obtained by sniffing serial packets.  Most of the function codes are not understood.
 
-  message[0]= 0x02;                   //byte 0  = Start of message.  Always 0x02
-  message[1]= (ACaddr[AC] >> 8);  //byte 1  = receiver node address byte 1
-  message[2]= (ACaddr[AC]);       //byte 2  = receiver node address byte 2
-  message[3]= (node_this >> 8);         //byte 3  = transmitter node address byte 1
-  message[4]= (node_this);              //byte 4  = transmitter node address byte 2
-  message[5]= 0x91;                   //byte 5  = function code. Always 0x91
-  message[6]= 0x77;                   //byte 6  = function code. Always 0x77
-  message[7]= 0x02;                   //byte 7  = handle start. Always 0x02
-  message[8]= 0x05;                   //byte 8  = input object. Always 0x05 for an input.  Change as required for other types.
-  message[9]= byte (ACinst[AC]);       //byte 9  = instance. Note that parsing "0x00" to byte is probably not necessary, but removes abiguity with NULL character.
-  message[10]= byte(0x00);            //byte 10 = attribute. Always 0x00 for an input.  Note that parsing "0x00" to byte removes abiguity with NULL character.
-  message[11]= 0x01;                  //byte 11 = select. Always 0x01 for an input.
-  message[12]= 0x01;                  //byte 12 = unknown. Always 0x01
-  message[13]= 0x02;                  //byte 13 = handle end. Usually 0x02. Response returns 0x00 if handle not valid.
+  message[0]= 0x02;              //byte 0  = Start of message.  Always 0x02
+  message[1]= (ACaddr[AC] >> 8); //byte 1  = receiver node address byte 1
+  message[2]= (ACaddr[AC]);      //byte 2  = receiver node address byte 2
+  message[3]= (node_this >> 8);  //byte 3  = transmitter node address byte 1
+  message[4]= (node_this);       //byte 4  = transmitter node address byte 2
+  message[5]= 0x91;              //byte 5  = function code. Always 0x91
+  message[6]= 0x77;              //byte 6  = function code. Always 0x77
+  message[7]= 0x02;              //byte 7  = handle start. Always 0x02
+  message[8]= 0x05;              //byte 8  = input object. Always 0x05 for an input.  Change as required for other types.
+  message[9]= byte (ACinst[AC]); //byte 9  = instance. Note that parsing "0x00" to byte is probably not necessary, but removes abiguity with NULL character.
+  message[10]= byte(0x00);       //byte 10 = attribute. Always 0x00 for an input.  Note that parsing "0x00" to byte removes abiguity with NULL character.
+  message[11]= 0x01;             //byte 11 = select. Always 0x01 for an input.
+  message[12]= 0x01;             //byte 12 = unknown. Always 0x01
+  message[13]= 0x02;             //byte 13 = handle end. Usually 0x02. Response returns 0x00 if handle not valid.
 
   checkSum = 0;
   for (int x = 0; x < 14; x++)
@@ -610,8 +555,6 @@ int BUILD_QUERY (int indexNOW, byte AC)
   return incrementCaseValue;
 }
 
-
-
 void serialFlush()
 //Procedure to clear all characters from the hardware serial buffer.
 {
@@ -620,7 +563,6 @@ void serialFlush()
     char t = Serial.read();
   }
 }
-
 
 void ASICserialFlush()
 //Procedure to clear all characters from the Software Serial buffer.
